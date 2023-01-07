4fabe2fbfe5bb3072346dbe423b725e18d14dba1
elizaveta72305 <elizaveta72305@gmail.com>
2023-01-07 14:03:30 +0200

    Добавьте файлы проекта.

---
+++ Models/StatusModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace WebAssemblyF.Models;
+
+	public class StatusModel
+	{
+		[BsonId, BsonRepresentation(BsonType.ObjectId)]
+
+		public string? StatusId { get; set; }
+
+		[BsonElement("status"), BsonRepresentation(BsonType.String)]
+		public string Status { get; set; }
+
+	}
+
+
