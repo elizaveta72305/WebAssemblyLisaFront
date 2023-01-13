c43041eb341b4561d1cf36cd0d7ffce6531684c2
elizaveta72305 <elizaveta72305@gmail.com>
2023-01-13 12:34:28 +0200

    delete useless code

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -11,9 +11,6 @@ using System.Diagnostics;
 using Newtonsoft.Json.Linq;
 using MongoDB.Bson.IO;
 using System.Text.RegularExpressions;
-
-//using Newtonsoft.Json;
-
 namespace WebAssemblyF.Pages;
 
 	[Authorize]
@@ -25,22 +22,17 @@ namespace WebAssemblyF.Pages;
 
 		public string AccessToken { get; set; }
 
-	protected override async Task OnInitializedAsync()
-	{
-		var accessTokenResult = await TokenProvider.RequestAccessToken();
-		var AccessToken = string.Empty;
+	//protected override async Task OnInitializedAsync()
+	//{
+	//	var accessTokenResult = await TokenProvider.RequestAccessToken();
+	//	var AccessToken = string.Empty;
 
-		if (accessTokenResult.TryGetToken(out var token))
-		{
-			AccessToken = token.Value;
-			var handler = new JwtSecurityTokenHandler();
-			var jwtSecurityToken = handler.ReadJwtToken(AccessToken);
-			var mail = jwtSecurityToken.Claims.First(c => c.Type == "mail").Value;
-
-			//var claim = token.Claims.First(c => c.Type == "email").Value;
-			//return claim;
-
-		}
-		
-	}
+	//	if (accessTokenResult.TryGetToken(out var token))
+	//	{
+	//		AccessToken = token.Value;
+	//		var handler = new JwtSecurityTokenHandler();
+	//		var jwtSecurityToken = handler.ReadJwtToken(AccessToken);
+	//		var mail = jwtSecurityToken.Claims.First(c => c.Type == "mail").Value;
+	//	}
+	
 }
