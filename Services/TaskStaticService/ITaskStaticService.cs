7b07af5387ea9006fc5f7ad42e7b48674815b244
Khazaar <eeguar@gmail.com>
2023-01-16 18:54:33 +0200

    Statr

---
+++ Services/TaskStaticService/ITaskStaticService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebAssemblyF.Models;
+namespace WebAssemblyF.Services.TaskStaticService
+{
+	public interface ITaskStaticService
+	{
+        public List<ITaskStatic> tasksStatic { get; set; }
+        public List<string> categories { get; set; }
+        public Task GetTasksStatic();
+        public Task<ITaskStatic> GetSingleTaskStatic(string id);
+        public Task CreateTaskStatic(ITaskStatic taskStatic);
+        public Task UpdateTaskStatic(ITaskStatic taskStatic);
+        public Task DeleteTaskStatic(string id);
+    }
+}
+
