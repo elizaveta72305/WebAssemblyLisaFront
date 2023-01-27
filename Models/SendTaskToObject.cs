05ce331887de143c501f52f1d4534a2a7d57548c
elizaveta72305 <elizaveta72305@gmail.com>
2023-01-27 15:28:48 +0200

    fix non-unique input

---
+++ Models/SendTaskToObject.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAssemblyF.Models
+{
+	public class SendTaskToObject
+	{
+		public ITaskDynamic taskOpen { get; set; }
+		public new List<string> collaborators { get; set; }
+
+		public string input { get; set; }
+
+		public SendTaskToObject(ITaskDynamic taskOpen, List<string> collaborators, string input)
+		{
+			this.taskOpen = taskOpen;
+			this.collaborators = collaborators;
+			this.input = input;
+		}
+	}
+}
--- Models/SubmitTaskBody.cs
+++ Models/SubmitTaskBody.cs
@@ -2,15 +2,18 @@
 {
 	public class SubmitTaskBody
 	{
-			public string taskName { get; set; }
+		public string teamName { get; set; }
+
+		public string taskName { get; set; }
 			public string solution { get; set; }
 
 			//public string email { get; set; }
 
-			public SubmitTaskBody(string taskName, string solution)
+			public SubmitTaskBody(string teamName, string taskName, string solution)
 			{
 				this.taskName = taskName;
 				this.solution = solution;
+				this.teamName = teamName;
 				//this.email = email;
 			}
 	}
